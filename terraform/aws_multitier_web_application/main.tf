877cfdbfef62534767eafa1e5718834282fe27de
Julian Beck <ju-fa-beck@t-online.de>
2021-08-25 21:27:18 +0200

    traefik

--- terraform/aws_multitier_web_application/main.tf
+++ terraform/aws_multitier_web_application/main.tf
@@ -1,10 +1,18 @@
 module "autoscaling" {
-  source    = "./modules/autoscaling"
-  namespace = var.namespace
+  source      = "./modules/autoscaling"
+  namespace   = var.namespace
+  ssh_keypair = var.ssh_keypair
+  vpc         = module.networking.vpc
+  sg          = module.networking.sg
+  db_config   = module.database.db_config
 }
 module "database" {
   source    = "./modules/database"
   namespace = var.namespace
+  #Pass output variables from the 
+  #Network module into the database module
+  vpc = module.networking.vpc
+  sg  = module.networking.sg
 }
 module "networking" {
   source    = "./modules/networking"
---
+++ traefik/README.md
@@ -0,0 +1,8 @@
+[Source](https://github.com/DoTheEvo/Traefik-v2-examples#1-traefik-routing-to-various-docker-containers)
+
+```
+docker network create traefik_net
+```
+```
+docker network inspect traefik_net
+```

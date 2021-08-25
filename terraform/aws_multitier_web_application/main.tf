d79496e9b70269b7e29343dd3a9eac6c14975eb5
Julian Beck <ju-fa-beck@t-online.de>
2021-08-25 14:05:13 +0200

    minor

---
+++ terraform/aws_multitier_web_application/main.tf
@@ -0,0 +1,12 @@
+module "autoscaling" {
+  source    = "./modules/autoscaling"
+  namespace = var.namespace
+}
+module "database" {
+  source    = "./modules/database"
+  namespace = var.namespace
+}
+module "networking" {
+  source    = "./modules/networking"
+  namespace = var.namespace
+}

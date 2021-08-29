aee8b105e7b8d3bc3a41d26632b8b3ba1b7c79ff
Julian Beck <ju-fa-beck@t-online.de>
2021-08-29 09:34:33 +0200

    change java version

--- terraform/ec2_minecraft_server/main.tf
+++ terraform/ec2_minecraft_server/main.tf
@@ -18,8 +18,13 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-locals {
 
+variable "ssh_public_key_path" {
+  description = "SSH public key file path"
+  default     = "~/.ssh/id_rsa.pub"
+}
+resource "aws_key_pair" "public_key" {
+  public_key = file(var.ssh_public_key_path)
 }
 
 resource "aws_instance" "minecraft_server" {
@@ -28,6 +33,7 @@ resource "aws_instance" "minecraft_server" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data              = filebase64("${path.module}/templates/user_data.tpl")
+  key_name               = aws_key_pair.public_key.id
 
   tags = {
     Name = "Minecraft-Server"
@@ -37,21 +43,24 @@ resource "aws_instance" "minecraft_server" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 25565
+    to_port     = 25565
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } 
+  # for ssh server access
   ingress {
-    from_port   = 25565
-    to_port     = 25565
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
+
+  # Enable all egress traffic
   egress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- terraform/ec2_minecraft_server/templates/user_data.tpl
+++ terraform/ec2_minecraft_server/templates/user_data.tpl
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt install -y openjdk-8-jre-headless wget
+sudo apt-get update && sudo-apt upgrade -y
+sudo apt install -y openjdk-16-jre-headless wget
 sudo mkdir /minecraft
-sudo chown -R ubuntu:ubuntu /minecraft
 cd /minecraft
-wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar -O minecraft_server.1.17.jar
+wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -O minecraft_server.1.17.jar
 echo '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
 #Mon Aug 06 18:11:14 UTC 2018
 eula=true' > eula.txt
@@ -12,9 +12,12 @@ sudo cat <<EOF > /etc/systemd/system/minecraft.service
 Description=Minecraft Service
 After=default.target
 [Service]
+WorkingDirectory=/minecraft/
 Type=simple
-User=ec2-user
+User=ubuntu
+Group=ubuntu
 ExecStart=/usr/bin/java -jar /minecraft/minecraft_server.1.17.jar nogui
 EOF
+sudo chown -R ubuntu:ubuntu /minecraft
 sudo systemctl daemon-reload
 sudo service minecraft start

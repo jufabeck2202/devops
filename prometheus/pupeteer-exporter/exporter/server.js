cf17f36b04b81c34f30540e0b51768bf035b71e3
Julian Beck <ju-fa-beck@t-online.de>
2021-09-19 18:02:19 +0000

    basic mvp

---
+++ prometheus/pupeteer-exporter/exporter/server.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+async function helloWorld() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-gpu"],
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://en.wikipedia.org/wiki/%22Hello,_World!%22_program");
+
+  const firstPar = await page.$eval("#mw-content-text p", (el) => el.innerText);
+
+  console.log(firstPar); // A "Hello, World!" program is a computer program that outputs ...
+
+  await browser.close();
+}
+
+helloWorld();
